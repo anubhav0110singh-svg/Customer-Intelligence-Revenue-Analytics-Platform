69a341927ad35c1b8e21f84f70ad9f06fb8ee029
anubhav0110singh-svg <anubhav0110singh@gmail.com>
2026-09-18 23:03:48 +0530

    01_kpi_analysis.sql

--- sql/01_kpi_analysis.sql
+++ sql/01_kpi_analysis.sql
@@ -1,1 +1,11 @@
+-- Customer Intelligence & Revenue Analytics
+-- Overall KPI Analysis
 
+SELECT
+    COUNT(*) AS total_customers,
+    SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) AS churned_customers,
+    ROUND(
+        SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        2
+    ) AS churn_rate_percentage
+FROM silver_customer;
